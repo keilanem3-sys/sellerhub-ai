af8d7aed34aa2ff2b0c6f991886dafc0d747128a
keilanem3-sys <keilanem3@gmail.com>
2025-12-18 19:32:52 -0300

    Update server.js

--- scr/server.js
+++ scr/server.js
@@ -3,35 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-
 app.get("/", (req, res) => {
-  res.send("Servidor online 🚀");
-});
-
-app.get("/test-db", async (req, res) => {
-  try {
-    const { Pool } = require("pg");
-
-    const pool = new Pool({
-      host: process.env.PGHOST,
-      port: process.env.PGPORT,
-      user: process.env.PGUSER,
-      password: process.env.PGPASSWORD,
-      database: process.env.PGDATABASE,
-      ssl: { rejectUnauthorized: false }
-    });
-
-    const result = await pool.query("SELECT NOW()");
-    res.json(result.rows[0]);
-  } catch (err) {
-    res.status(500).json({ erro: err.message });
-  }
+  res.send("Servidor rodando com sucesso 🚀");
 });
 
 app.listen(PORT, () => {
-  console.log("Servidor rodando na porta", PORT);
+  console.log("Servidor iniciado na porta", PORT);
 });
-"scripts": {
-  "start": "node server.js"
-}

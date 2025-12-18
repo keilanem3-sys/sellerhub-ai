b5552dffc80eb32e22da104aed236a92d698f848
keilanem3-sys <keilanem3@gmail.com>
2025-12-18 05:19:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("App rodando com sucesso 🚀");
+  res.send("Servidor funcionando 🚀");
 });
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(Servidor iniciado na porta ${PORT});
 });

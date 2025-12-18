ff5831330f681c5a81c566ed774b49e623ec06a9
keilanem3-sys <keilanem3@gmail.com>
2025-12-18 19:17:41 -0300

    Update health.js

--- scr/routes/health.js
+++ scr/routes/health.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../db");
 
-router.get("/health", (req, res) => {
-  res.json({
-    status: "ok",
-    message: "Servidor saudável 🚀"
-  });
+router.get("/", (req, res) => {
+  res.send("API rodando 🚀");
+});
+
+router.get("/test-db", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.json({
+      status: "ok",
+      hora: result.rows[0]
+    });
+  } catch (err) {
+    res.status(500).json({
+      erro: err.message
+    });
+  }
 });
 
 module.exports = router;

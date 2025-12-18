add264261a19fe67f9599a006117764f75077115
keilanem3-sys <keilanem3@gmail.com>
2025-12-18 17:24:35 -0300

    Update server.js

--- scr/server.js
+++ scr/server.js
@@ -1,1 +1,17 @@
+import express from "express";
 
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    app: "SellerHub AI",
+    status: "online",
+    message: "Servidor rodando com sucesso 🚀"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(Servidor iniciado na porta ${PORT});
+});

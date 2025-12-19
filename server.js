6ad3754018fcab5e7d992437d8b1803ed285a034
keilanem3-sys <keilanem3@gmail.com>
2025-12-19 10:17:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -23,3 +23,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor rodando na porta", PORT);
 });
+app.get("/status", (req, res) => {
+  res.json({ status: "ok", servidor: "online" });
+});

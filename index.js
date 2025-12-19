af777c1e2887040ceffe1ce6f3a18a61a725bac0
keilanem3-sys <keilanem3@gmail.com>
2025-12-19 19:21:21 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-const express = require("express");
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("App rodando com sucesso 🚀");
-});
-
-app.listen(PORT, () => {
-  console.log(Servidor iniciado na porta ${PORT});
-});
-require("./escr/servidor");
-console.log(Servidor iniciado na porta ${PORT});
-"scripts": {
-  "start": "node index.js"
-}
+// arquivo não utilizado

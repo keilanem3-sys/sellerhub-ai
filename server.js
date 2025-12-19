09e210a8aed3555bec1856800753be240b3770e6
keilanem3-sys <keilanem3@gmail.com>
2025-12-18 22:57:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
 
+// importar rotas
+const statusRoutes = require("./routes/status");
+
+// porta correta para Railway
 const PORT = process.env.PORT || 8080;
 
+// rota principal
 app.get("/", (req, res) => {
   res.send("Servidor rodando 🚀");
 });
 
+// usar rotas
+app.use("/", statusRoutes);
+
+// iniciar servidor
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor iniciado na porta", PORT);
 });

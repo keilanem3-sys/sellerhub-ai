f3d054139238d561c0be5c976d64a03a754c6cbc
keilanem3-sys <keilanem3@gmail.com>
2025-12-19 09:58:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const { Pool } = require("pg");
+// const { Pool } = require("pg");
 
 const app = express();
 app.use(express.json());
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+ //  ssl: { rejectUnauthorized: false }
+// });
 
 app.get("/health", (req, res) => {
   res.send("API rodando 🚀");

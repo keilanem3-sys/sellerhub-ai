6c8384559ffb3f81fcf817e430079e1cd518bab1
keilanem3-sys <keilanem3@gmail.com>
2025-12-18 22:27:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const app = express();
 
-const statusRoutes = require("./routes/status");
-
-// 🚨 NUNCA use porta fixa no Railway
-const PORT = process.env.PORT;
+const PORT = Number(process.env.PORT);
 
 app.get("/", (req, res) => {
   res.send("Servidor rodando 🚀");
 });
 
-app.use("/", statusRoutes);
-
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Servidor iniciado na porta " + PORT);
+  console.log("Servidor iniciado na porta", PORT);
 });

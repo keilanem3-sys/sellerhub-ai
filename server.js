b6f1ad2717bc274acc912beb512252644665eb81
keilanem3-sys <keilanem3@gmail.com>
2025-12-19 19:07:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -88,5 +88,4 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(Servidor rodando na porta ${PORT});
-});
+ console.log(Servidor rodando na porta ${PORT});

7b009b69d8dd64541a0c142299cda2751155c983
keilanem3-sys <keilanem3@gmail.com>
2025-12-18 21:41:00 -0300

    Update status.js

--- routes/status.js
+++ routes/status.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/status", (req, res) => {
   res.json({
-    status: "ok",
-    message: "API online 🚀"
+    online: true,
+    mensagem: "API funcionando 🚀"
   });
 });
 

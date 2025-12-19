d2558da84cb9aa72f0479cfabe9098aa326f32aa
keilanem3-sys <keilanem3@gmail.com>
2025-12-18 21:36:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 // importar rotas
-const statusRoutes = require("./rotas/status");
+const statusRoutes = require("./routes/status");
 
 const PORT = process.env.PORT || 3000;
 

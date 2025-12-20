3281c00832b92c13df907303096a07d25e1c9151
keilanem3-sys <keilanem3@gmail.com>
2025-12-19 21:16:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // ROTA DE SAÚDE (OBRIGATÓRIA NO RAILWAY)
 app.get("/", (req, res) => {

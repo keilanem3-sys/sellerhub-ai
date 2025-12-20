a4403803969a844a6a82aa25bc2d155fa79d7691
keilanem3-sys <keilanem3@gmail.com>
2025-12-20 11:12:24 -0300

    Update server.js

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ app.get("/users", (req, res) => {
 
 // 🚀 SERVIDOR
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(Servidor rodando na porta ${PORT});
+  console.log(`Servidor rodando na porta ${PORT}`);
 });

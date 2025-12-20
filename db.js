d55380f45287f3bb89e815c53b155a0d2eec3006
keilanem3-sys <keilanem3@gmail.com>
2025-12-20 20:40:00 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_PUBLIC_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
+
+pool
+  .connect()
+  .then(() => console.log("✅ Banco conectado com sucesso"))
+  .catch((err) => console.error("❌ Erro ao conectar no banco:", err));
 
 module.exports = pool;

8ea234205ef193b674e0712d3939549ba366f7a0
keilanem3-sys <keilanem3@gmail.com>
2025-12-20 10:09:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get("/", (req, res) => {
   res.status(200).send("API SellerHub rodando 🚀");
 });
 
+app.get("/status", (req, res) => {
+  res.json({
+    status: "ok",
+    app: "SellerHub",
+    online: true
+  });
+});
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });

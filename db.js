c9a4c3366347f41c16bedf2f96a19b3b0c699df6
keilanem3-sys <keilanem3@gmail.com>
2025-12-20 16:35:43 -0300

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,9 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.URL_DO_BANCO_DE_DADOS,
-  ssl: { rejectUnauthorized: false }
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
